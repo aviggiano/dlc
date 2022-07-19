6d2fa551de7911cb30597f463bc497e8a792473e
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2022-07-19 17:29:21 -0300

    Download in headless mode

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,37 @@ import { promises as fs } from 'fs'
 const URL = process.argv[2]
 const DIR = process.argv[3] || 'contracts/'
 
+const COPY_TO_CLIPBOARD_BUTTONS = '.js-clipboard.mr-1'
+const FILENAME_SPANS =
+  'div.d-flex.justify-content-between > span.text-secondary'
+
 export async function download(url: string, dir: string): Promise<void> {
   console.log(`download '${url}' and save to '${dir}'`)
-  const browser = await puppeteer.launch({ headless: false })
+  const browser = await puppeteer.launch()
   const context = browser.defaultBrowserContext()
   context.overridePermissions(url, ['clipboard-read'])
   const page = await browser.newPage()
-  await page.goto(url)
-  const copyToClipboardButtons = await page.$$('.js-clipboard.mr-1')
+  await page.setUserAgent(
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+  )
+  await page.setViewport({ width: 960, height: 768 })
+  page.goto(url)
+  await Promise.race([
+    page.waitForNavigation({ waitUntil: 'domcontentloaded' }),
+    page.waitForNavigation({ waitUntil: 'load' }),
+  ])
+  await page.waitForSelector(COPY_TO_CLIPBOARD_BUTTONS)
+  const copyToClipboardButtons = await page.$$(COPY_TO_CLIPBOARD_BUTTONS)
+
+  await page.waitForSelector(FILENAME_SPANS)
   const filenames = (
-    await page.evaluate(() =>
-      Array.from(
-        document.querySelectorAll(
-          'div.d-flex.justify-content-between > span.text-secondary'
+    await page.evaluate(
+      (selector) =>
+        Array.from(
+          document.querySelectorAll(selector),
+          (element) => element.innerHTML
         ),
-        (element) => element.innerHTML
-      )
+      FILENAME_SPANS
     )
   )
     .map((text) => text.match(/File.*: (.*)/)?.[1])
@@ -34,7 +49,7 @@ export async function download(url: string, dir: string): Promise<void> {
     const filename = filenames[i]
     i += 1
     if (filename) {
-      await button.click()
+      await button.evaluate((b) => (b as any).click())
       const code = await page.evaluate(() => navigator.clipboard.readText())
 
       console.log(`Saving file ${filename}`)
